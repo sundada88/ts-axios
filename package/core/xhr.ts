93ee73c71a0d63c61a086e723e56ed7fa214f627
sundada <956593305@qq.com>
2022-08-17 09:00:35 +0800

    add handle error

--- package/core/xhr.ts
+++ package/core/xhr.ts
@@ -2,11 +2,20 @@ import { parseHeaders } from "../helpers/headers";
 import { AxiosPromise, AxiosRequestConfig, AxiosResponse } from "../types";
 
 export function xhr(config: AxiosRequestConfig):AxiosPromise {
-  return new Promise(resolve => {
-    const {method, url, data, headers, responseType } = config
+  return new Promise((resolve, reject) => {
+    const {method, url, data, headers, responseType, timeout } = config
     const request = new XMLHttpRequest()
+    if (timeout) {
+      request.timeout = timeout
+    }
     if (responseType) {
       request.responseType = responseType
+    }
+    request.onerror = function handleError() {
+      reject(new Error('Network error!'))
+    }
+    request.ontimeout = function handleTimeout() {
+      reject(new Error(`Timeout of ${timeout} ms exceeded`))
     }
     request.open(method.toUpperCase(), url, true)
     Object.keys(headers).forEach(name => {
@@ -18,6 +27,7 @@ export function xhr(config: AxiosRequestConfig):AxiosPromise {
     })
     request.onreadystatechange =  function handleLoad() {
       if (request.readyState !== 4) return
+      if (request.status === 0) return
       const responseHeaders = parseHeaders(request.getAllResponseHeaders())
       const responseData = responseType && responseType !== 'text' ? request.response : request.responseText
       const response: AxiosResponse = {
@@ -28,9 +38,17 @@ export function xhr(config: AxiosRequestConfig):AxiosPromise {
         config,
         request
       }
-      resolve(response)
+      // resolve(response)
+      handleResponse(response)
 
     }
     request.send(data)
+    function handleResponse (response: AxiosResponse) {
+      if (response.status >= 200 && response.status < 300) {
+        resolve(response)
+      } else {
+        reject(new Error(`Request failed with status code ${response.status}`))
+      }
+    }
   })
 }
