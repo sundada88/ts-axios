ae3f7d958f3ca95d3697b8fcc9be997899619a75
sundada <956593305@qq.com>
2022-09-01 09:24:07 +0800

    add axios.create function

--- src/axios.ts
+++ src/axios.ts
@@ -1,13 +1,17 @@
 import Axios from './core/axios'
 import defaults from './core/defaults'
+import { mergeConfig } from './core/mergeConfig'
 import { extend } from './helpers/utils'
-import { AxiosInstance, AxiosRequestConfig } from './types'
+import { AxiosRequestConfig, AxiosStatic } from './types'
 
-function createInstance (config: AxiosRequestConfig): AxiosInstance {
+function createInstance (config: AxiosRequestConfig): AxiosStatic {
   const context = new Axios(config)
   const instance = Axios.prototype.request.bind(context)
   extend(instance, context)
-  return instance as AxiosInstance
+  return instance as AxiosStatic
 }
 const axios = createInstance(defaults)
+axios.create = function create (config) {
+  return createInstance(mergeConfig(defaults, config))
+}
 export default axios
